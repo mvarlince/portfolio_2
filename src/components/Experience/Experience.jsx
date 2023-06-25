4d1bb97a2ea097e2d11d79bce372df39c06c350c
Varlince Meran <varlincemeran@gmail.com>
2023-06-25 13:05:23 -0400

    added github and techstack buttons

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -17,6 +17,16 @@ export default function Experience() {
             </h2>
           </Col>
         </Row>
+        
+        <Row sm={8} lg={3} className="justify-content-center g-5">
+          <h3 >
+            Jr Programmer - C# .NET Developer
+            <span className="d-md-block"></span>
+          </h3>
+          <p>
+            DebtHelper <br></br> July 2023 - Present
+          </p>
+        </Row>
 
         <Row sm={8} lg={3} className="justify-content-center g-5">
             <h3 className="text-center">
@@ -25,7 +35,7 @@ export default function Experience() {
             </h3>
             <p>
               {" "}
-              Boca Code <br></br> March 2023 - Present
+              Boca Code <br></br> March 2023 - June 2023
             </p>
           </Row>
 
@@ -37,15 +47,6 @@ export default function Experience() {
               {" "}
               Anthology Inc. + Blackboard <br></br> May 2019 - March 2022
             </p>
-            {/* <p className="text-center text-md-start">
-
-              As a software support analyst, I managed 8+ client accounts and ranked in the top 5 for incident resolution. 
-              I troubleshooted errors and data issues, reproduced
-              defects, and worked with cross-functional teams to configure an
-              enterprise Azure-based application. <br></br> Additionally, I managed
-              customer support accounts for efficient incident routing and app
-              upgrades/releases.
-            </p> */}
           </Row>
 
           <Row sm={8} lg={3} className="justify-content-center g-5">
@@ -57,17 +58,6 @@ export default function Experience() {
               {" "}
               Honorlock Proctoring <br></br> April 2018 - May 2019
             </p>
-            {/* <p className="text-center text-md-start">
-              As an application support specialist, I resolved
-              high-priority customer issues through live chats, emails, and
-              phone calls. I also mentored tier 1 agents with handling
-              complex proctoring tasks to ensure the smooth delivery of
-              proctoring services.<br></br> 
-              I collaborated with management and engineering teams
-              to complete critical tasks such as data analysis, bug reporting,
-              manual software testing, and information security training
-              programs. 
-            </p> */}
           </Row>
 
           <Row sm={8} lg={3} className="justify-content-center g-5">
@@ -78,16 +68,6 @@ export default function Experience() {
               {" "}
               Florida Atlantic University <br></br> January 2017 - May 2018
             </p>
-            {/* <p className="text-center text-md-start">
-              As a technical assistant, I led the course migration to a
-              cloud-based Learning Management System and provided immediate
-              technical support. I also resolved computer access, password,
-              printer, and software issues for faculty, team members, and
-              managers. <br></br> 
-              In addition, I worked on various projects assigned by
-              management, including media coordination, video production, and
-              data analytics.
-            </p> */}
           </Row>
 
           <Row sm={8} lg={3} className="justify-content-center g-5">
@@ -99,16 +79,7 @@ export default function Experience() {
               Broward College - Campus Technology <br></br> August 2016 -
               December 2016
             </p>
-            {/* <p className="text-center text-md-start">
-              As an IT intern, I served as the primary point of contact for
-              faculty seeking technical assistance through various channels. I
-              provided technical support to technicians by installing,
-              configuring, moving, and troubleshooting hardware and software
-              issues. <br></br> 
-              Additionally, I proactively monitored the Service Desk
-              ticket queue, efficiently resolving tickets and managing
-              escalations.
-            </p> */}
+            
           </Row>
           <Button
                 size="lg"
--- src/components/Portfolio/Panel.jsx
+++ src/components/Portfolio/Panel.jsx
@@ -1,28 +1,45 @@
-import { Col, Card } from "react-bootstrap";
+import { Col, Card, Button, Row } from "react-bootstrap";
 
-export default function Panel ({ data:{id, name, about, website, cover} }) {
-
+export default function Panel({ data: { id, name, about, website, cover } }) {
   // const handleSiteLaunch = websiteUrl => {
   //   console.log(websiteUrl);
   //   window.open(websiteUrl, "_blank");
   // }
-  
+
   return (
     <Col key={id} md={10} lg={6} xl={4}>
       <Card className="portfolio-panel button-effect ">
         <figure>
-          <a href={website} 
-             target="_blank"
-             rel="noreferrer"><Card.Img
-            src={`/images/${cover}`} /></a>
+          <a href={website} target="_blank" rel="noreferrer">
+            <Card.Img src={`/images/${cover}`} />
+          </a>
         </figure>
 
-        <div className="content">
+        <div className="content ">
           <h3>{name}</h3>
           <p>{about}</p>
-          {/* <button> TechStack </button> */}
+          <Row>
+            <Col>
+              <Button
+                size="lg"
+                variant="outline-danger"
+                className="button-style-links button-effect"
+              >
+                Code in Github
+              </Button>
+            </Col>
+            <Col>
+            <Button
+              size="lg"
+              variant="outline-danger"
+              className="button-style-links button-effect"
+              >
+              TechStack
+            </Button>
+            </Col>
+          </Row>
         </div>
       </Card>
     </Col>
-  )
+  );
 }
